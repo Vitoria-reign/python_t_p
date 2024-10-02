5dcda913af7b26c59f3ebbb8035aea96e184b093
vitoria-reinado <vitoria.duarte880@gmail.com>
2024-10-02 20:03:47 -0300

    Repetição while

---
+++ estruturas_repeticao1.py
@@ -0,0 +1,6 @@
+entrada_idade = ""
+while entrada_idade != "0":
+    entrada_idade = str(input("Digite um numero qualquer ou 0 para sair: \n"))
+    print("O numero digitado é: ", entrada_idade)
+
+
